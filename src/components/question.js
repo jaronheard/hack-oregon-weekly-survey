5eeb2718562d8faa80cf06fc0742cc5a0203ef6a
Jaron Heard <jaronheard@gmail.com>
2019-05-13 01:31:22 -0700

    added multi-selector

---
+++ src/components/question.js
@@ -0,0 +1,34 @@
+import React, { Fragment } from "react"
+
+import { rhythm } from "../utils/typography"
+
+function Question({question, answer}) {
+  return (
+    <Fragment>
+      <h3
+        style={{
+          marginTop: rhythm(1 / 2),
+          marginBottom: rhythm(1 / 2),
+        }}
+      >
+        {question}
+      </h3>
+      <section>
+        {answer.split("\n").map((item, key) => {
+          return (
+            <p
+              style={{
+                marginBottom: rhythm(1 / 4),
+              }}
+              key={key}
+            >
+              {item}
+            </p>
+          )
+        })}
+      </section>
+    </Fragment>
+  )
+}
+
+export default Question
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,88 +4,151 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Question from "../components/question"
 import { rhythm } from "../utils/typography"
 
+import { withStyles } from "@material-ui/core/styles"
+import Input from "@material-ui/core/Input"
+import MenuItem from "@material-ui/core/MenuItem"
+import FormControl from "@material-ui/core/FormControl"
+import Select from "@material-ui/core/Select"
 
+const styles = theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+    maxWidth: 300,
+  },
+  chips: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  chip: {
+    margin: theme.spacing.unit / 4,
+  },
+  noLabel: {
+    marginTop: theme.spacing.unit * 3,
+  },
+})
+
+const ITEM_HEIGHT = 48
+const ITEM_PADDING_TOP = 8
+
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+}
+
+function getStyles(name, that) {
+  return {
+    fontWeight:
+      that.state.selectedTeams.indexOf(name) === -1
+        ? that.props.theme.typography.fontWeightRegular
+        : that.props.theme.typography.fontWeightMedium,
+  }
+}
 
 class BlogIndex extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { selectedTeams: ["Design"] }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange = event => {
+    this.setState({ selectedTeams: event.target.value })
+  }
+
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
-    const posts = data.allGoogleSheetMostRecentRow.edges
+    const allQuestions = data.allGoogleSheetMostRecentRow.edges
+    const teams = allQuestions.map(({ node }) => node.team)
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
-          title="All posts"
+          title="All Questions"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
         <Bio />
-        {posts.map(({ node }) => {
-          const title = node.team
-          return (
-            <div key={node.team}>
-              <h2
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                {title}
-              </h2>
-              <small>{node.date}</small>
-              <p>
-                <strong>What did your team do this week?</strong>
-              </p>
-              <section>
-                {node.whatdidyourteamdothisweek
-                  .split("\n")
-                  .map((item, key) => {
-                    return <p key={key}>{item}</p>
-                  })}
-              </section>
-              <p>
-                <strong>
-                  What is your team going to do next week?
-                </strong>
-              </p>
-              <section>
-                {node.whatisyourteamgoingtodonextweek
-                  .split("\n")
-                  .map((item, key) => {
-                    return <p key={key}>{item}</p>
-                  })}
-              </section>
-              <p>
-                <strong>
-                  What does your team need to be successful?
-                </strong>
-              </p>
-              <section>
-                {node.whatdoyouneedtobesuccessful
-                  .split("\n")
-                  .map((item, key) => {
-                    return <p key={key}>{item}</p>
-                  })}
-              </section>
-              <p>
-                <strong>Any Roadblocks?</strong>
-              </p>
-              <section>
-                {node.anyroadblocks
-                  .split("\n")
-                  .map((item, key) => {
-                    return <p key={key}>{item}</p>
-                  })}
-              </section>
-            </div>
+        <div>
+        <FormControl>
+          <Select
+            multiple
+            displayEmpty
+            value={this.state.selectedTeams}
+            onChange={this.handleChange}
+            input={<Input id="select-multiple-placeholder" />}
+            renderValue={selected => {
+              if (selected.length === 0) {
+                return <em>Placeholder</em>
+              }
+
+              return selected.join(", ")
+            }}
+            MenuProps={MenuProps}
+          >
+            <MenuItem disabled value="">
+              <em>Placeholder</em>
+            </MenuItem>
+            {teams.map(team => (
+              <MenuItem key={team} value={team} style={getStyles(team, this)}>
+                {team}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        </div>
+        {allQuestions
+          .filter(({ node }) =>
+            this.state.selectedTeams.some(team => node.team === team)
           )
-        })}
+          .map(({ node }) => {
+            return (
+              <div key={node.team}>
+                <h2
+                  style={{
+                    marginBottom: rhythm(1 / 4),
+                  }}
+                >
+                  {node.team}
+                </h2>
+                <time>{node.date}</time>
+                <Question
+                  question="What did your team do this week?"
+                  answer={node.whatdidyourteamdothisweek}
+                />
+                <Question
+                  question="What is your team going to do next week?"
+                  answer={node.whatisyourteamgoingtodonextweek}
+                />
+                <Question
+                  question="What does your team need to be successful"
+                  answer={node.whatdoyouneedtobesuccessful}
+                />
+                <Question
+                  question="Any roadblocks?"
+                  answer={node.anyroadblocks}
+                />
+              </div>
+            )
+          })}
       </Layout>
     )
   }
 }
 
-export default BlogIndex
+export default withStyles(styles, { withTheme: true })(BlogIndex)
 
 export const pageQuery = graphql`
   query {
@@ -96,6 +159,7 @@ export const pageQuery = graphql`
     }
     allGoogleSheetMostRecentRow (
       sort: { fields: [date], order: DESC }
+      
     ) {
       edges {
         node {
